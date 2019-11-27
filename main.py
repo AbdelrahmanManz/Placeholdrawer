59d637e3c7df0b2bcc42e9e427dd4cc5bfe24c8a
mubbygn <mubbyashraf@gmail.com>
2019-11-27 17:50:27 +0200

    Environment Variables

--- main.py
+++ main.py
@@ -2,11 +2,16 @@ from flask import Flask, render_template, request, send_file, after_this_request
 import requests
 import shutil
 import os
+from os.path import join, dirname
+from dotenv import load_dotenv
 from pymongo import MongoClient
 from bson.binary import Binary
 from threading import Thread
 
-client = MongoClient("mongodb+srv://MubbyWW:MANGAfruit_1@placeholdrawer-qt9vz.mongodb.net/test?retryWrites=true&w=majority")
+dotenv_path = join(dirname(__file__), '.env')
+load_dotenv(dotenv_path)
+
+client = MongoClient(os.environ.get("URI"))
 db = client.test
 cache = db.cache
 
@@ -26,10 +31,10 @@ def home():
 
 @app.route('/cage/<w>/<h>')
 def cage(w,h):
-    #response = requests.get(f'https://www.placeimg.com/{w}/{h}/any', stream=True)
     cachechk = cache.find_one({"size": f'{w},{h}'})
     if cachechk is None:
         response = requests.get(f'https://www.placecage.com/{w}/{h}', stream=True)
+        # response = requests.get(f'https://www.placeimg.com/{w}/{h}/any', stream=True)
         with open('img.png', 'wb') as out_file:
             shutil.copyfileobj(response.raw, out_file)
         t1 = Thread(target=cacheinsert, args=(w,h,))
